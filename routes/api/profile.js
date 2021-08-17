60c08a3fdd2c51ab237f8156146313fa11f7242d
Andrew <ramettaandrewj@gmail.com>
2021-08-17 14:19:10 -0400

    added error message

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -134,6 +134,9 @@ router.get("/user/:user_id", async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
     res.status(500).send("Server Error");
   }
 });
