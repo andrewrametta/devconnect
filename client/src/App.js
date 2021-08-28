5d080a66c59564b4854fe6e94d4e1e74f3dcf87a
Andrew <ramettaandrewj@gmail.com>
2021-08-28 11:58:14 -0400

    add alert

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Landing from "./components/Landing/Landing";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
+import Alert from "./components/Alert/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -16,6 +17,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
